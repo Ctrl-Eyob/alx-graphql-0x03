81932c6e623417fc4a154ff0e315bb850eda9806
Ctrl-Eyob <eyobaberabekru@gmail.com>
2025-12-28 14:05:10 +0300

    error boundary component imported to pages

--- alx-rick-and-morty-app/pages/_app.tsx
+++ alx-rick-and-morty-app/pages/_app.tsx
@@ -2,6 +2,20 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ApolloProvider } from "@apollo/client";
 import client from "@/graphql/apolloClient";
+import ErrorBoundary from '@/components/ErrorBoundary';
+import type { AppProps } from "next/app";
+
+
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ErrorBoundary>
+      <Component {...pageProps} />
+    </ErrorBoundary>
+  );
+}
+
+export default MyApp;
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
